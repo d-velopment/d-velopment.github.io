abc400a1e0ed64f930886f02d0a4f7352bedb194
Dmitry Karlikov <dmitry.karlikov@relax-gaming.com>
2021-08-23 22:43:47 +0300

    Front-End view Header buttons update confirmed

--- iwish/svelte/src/components/Header.svelte
+++ iwish/svelte/src/components/Header.svelte
@@ -5,17 +5,7 @@
 <section class="features13 cid-shi8eTGcZg" id="features14-1">
   <div class="container">
     <div class="row">
-      <div class="card col-12 col-md-4 col-lg-2 p-3">
-        <div class="card-wrapper">
-          <div class="card-box align-center">
-            <a href="index.html"><span class="mbr-iconfont mobi-mbri-home mobi-mbri"></span></a>
-            <h4 class="card-title align-center mbr-black mbr-fonts-style display-7">
-              <strong>Главная</strong>
-            </h4>
-          </div>
-        </div>
-      </div>
-      <div class="card p-3 col-12 col-md-4 col-lg-2">
+      <div class="card p-0 col-3 col-md-3 col-lg-2">
         <div class="card-wrapper">
           <div class="card-box align-center">
             <a href="index.html"><span class="mbr-iconfont mobi-mbri-image-gallery mobi-mbri"></span></a>
@@ -23,7 +13,7 @@
           </div>
         </div>
       </div>
-      <div class="card p-3 col-12 col-md-4 col-lg-2">
+      <div class="card p-0 col-3 col-md-3 col-lg-2">
         <div class="card-wrapper">
           <div class="card-box align-center">
             <a href="index.html"><span class="mbr-iconfont mobi-mbri-gift mobi-mbri"></span></a>
@@ -31,7 +21,7 @@
           </div>
         </div>
       </div>
-      <div class="card p-3 col-12 col-md-4 col-lg-2">
+      <div class="card p-0 col-3 col-md-3 col-lg-2">
         <div class="card-wrapper">
           <div class="card-box align-center">
             <a href="index.html"><span class="mbr-iconfont mobi-mbri-features mobi-mbri"></span></a>
@@ -39,21 +29,11 @@
           </div>
         </div>
       </div>
-      <div class="card p-3 col-12 col-md-4 col-lg-2">
+      <div class="card p-0 col-3 col-md-3 col-lg-2">
         <div class="card-wrapper">
           <div class="card-box align-center">
             <a href="index.html"><span class="mbr-iconfont mobi-mbri-shopping-bag mobi-mbri"></span></a>
             <h4 class="card-title align-center mbr-black mbr-fonts-style display-7"><strong>Бронь</strong></h4>
-          </div>
-        </div>
-      </div>
-      <div class="card p-3 col-12 col-md-4 col-lg-2">
-        <div class="card-wrapper">
-          <div class="card-box align-center">
-            <a href="index.html"><span class="mbr-iconfont mobi-mbri-delivery mobi-mbri"></span></a>
-            <h4 class="card-title align-center mbr-black mbr-fonts-style display-7">
-              <strong>Напоминания</strong>
-            </h4>
           </div>
         </div>
       </div>
